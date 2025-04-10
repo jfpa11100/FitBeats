944cc99cd855d6a44b5b0e4fc3ed1fd71771ae4b
felipepala1000 <felipepala1000@gmail.com>
2025-04-10 00:25:29 +0000

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/Screens/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FitBeats',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomeScreen(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+// APIs TO HAVE IN MIND:
+// https://docs-en.kkbox.codes/#get-/tracks/{track_id}
+// https://developer.spotify.com/documentation/web-api/reference/get-track
